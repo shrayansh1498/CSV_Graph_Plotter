f225671b5f4722cbec96e2a8f8516179f7929a86
Shrayansh <47182109+shrayansh1498@users.noreply.github.com>
2023-11-10 18:42:37 +0530

    Delete CSV_Graph_Plotter_Project Version 2 directory

--- CSV_Graph_Plotter_Project Version 2/CSV_Graph_Plotter/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UploadedCSV
-
-class CSVUploadForm(forms.ModelForm):
-    class Meta:
-        model = UploadedCSV
-        fields = ['title', 'csv_file']
